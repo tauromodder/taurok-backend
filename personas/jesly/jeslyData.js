c85f17a6d0484037cda14ab34ec3b52d79469fdd
El Tauro Mods <eltauromods@gmail.com>
2026-07-14 06:28:25 -0500

    jeslyData.js

--- personas/jesly/jeslyData.js
+++ personas/jesly/jeslyData.js
@@ -3,8 +3,8 @@
 const jesly = {
   // Datos Personales
   nombreCompleto: "Jesly Jhajaira Roman Daga",
-  fechaNacimiento: "2006-08-02",
-  apodo: "Conejita 🐇",
+  fechaNacimiento: "2007-08-02",
+  apodo para jesly: "Conejita 🐇",
   edadActual: 18, // para julio 2026, próximamente 19
   signoZodiacal: "Leo",
   religion: "Cristiana Evangélica",
